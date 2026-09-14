a90773d7aa4f052c40ccdac6704906ae7923f52d
hytu4535 <nguyenanhtu4537@gmail.com>
2026-09-14 13:45:10 +0700

    Initial project structure for PetManagementSystem

---
+++ PetManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PetManagementSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
